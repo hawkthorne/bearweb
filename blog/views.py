1d09a1c92f813af26abfea60846345b3757e356a
Kyle Conroy <kyle@kyleconroy.com>
2013-12-13 15:55:30 -0800

    Make sure that missing blog pages don't 500

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,20 @@
 import os
 
 from django.views.generic import TemplateView
+from django.template.base import TemplateDoesNotExist
+from django.http import Http404
+from django.template.loader import get_template
 
 
 class ArticleView(TemplateView):
 
     def get_template_names(self):
         article_name = self.kwargs.get('article_name', 'notfound')
-        return [os.path.join('blog', 'blog', article_name, 'index.html')]
+        template = os.path.join('blog', 'blog', article_name, 'index.html')
+
+        try:
+            get_template(template)
+        except TemplateDoesNotExist:
+            raise Http404
+
+        return [template]
