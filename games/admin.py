d7ba563b24ba8c339cf1bb0af0f08a6f3a7c811b
Kyle Conroy <kyle@kyleconroy.com>
2013-12-14 00:20:01 -0800

    Deploy a task that causes an error
    We need to make sure that processing is alerting via Sentry

--- games/admin.py
+++ games/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
 from .models import Game, Framework, Release, Asset
+from games import tasks
+
+
+def make_published(modeladmin, request, queryset):
+    tasks.error.delay()
+make_published.short_description = "This causes an error"
 
 
 class GameAdmin(admin.ModelAdmin):
@@ -17,6 +23,7 @@ class ReleaseAdmin(admin.ModelAdmin):
 
 class AssetAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'release']
+    actions = [make_published]
 
 
 admin.site.register(Game, GameAdmin)
--- games/tasks.py
+++ games/tasks.py
@@ -4,5 +4,10 @@ from .bundle import package
 
 
 @task
+def error():
+    raise ValueError("Boop")
+
+
+@task
 def lovepackage(release_id):
     package(release_id)
