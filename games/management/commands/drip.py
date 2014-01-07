9916e4d088d0671e2d9187f6db5a8e9de0c1f019
Kyle Conroy <kyle@kyleconroy.com>
2014-01-07 09:43:53 -0800

    Add more commands for Customer IO backfill

---
+++ games/management/commands/drip.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.contrib.auth.models import User
+
+from games.models import Game
+from customerio import CustomerIO
+
+
+class Command(BaseCommand):
+    help = 'Sync current state into Customer IO'
+
+    def handle(self, *args, **options):
+        cio = CustomerIO(settings.CUSTOMERIO_SITE_ID,
+                         settings.CUSTOMERIO_API_KEY)
+
+        for user in User.objects.all():
+            cio.track(customer_id=user.pk, name='Sign Up')
+            self.stdout.write('SIGN UP {}'.format(user.username))
