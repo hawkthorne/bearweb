e54554a4d6e914c4591577f16cb12cca5678afc7
Kyle Conroy <kyle@kyleconroy.com>
2014-01-21 14:45:19 -0800

    Migrate to the new bucket

--- games/management/commands/s3sync.py
+++ games/management/commands/s3sync.py
@@ -10,8 +10,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         conn = S3Connection(settings.AWS_ACCESS_KEY_ID,
                             settings.AWS_SECRET_ACCESS_KEY)
-        source = conn.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
-        target = conn.get_bucket(settings.AWS_STORAGE_BUCKET_NEW)
+        source = conn.get_bucket(settings.AWS_STORAGE_BUCKET_OLD)
+        target = conn.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
 
         for idx, entry in enumerate(source.list()):
             if entry.name.endswith("/"):
@@ -20,7 +20,7 @@ class Command(BaseCommand):
                 self.stdout.write('COPY {}'.format(entry.name))
                 try:
                     target.copy_key(entry.name,
-                                    settings.AWS_STORAGE_BUCKET_NAME,
+                                    settings.AWS_STORAGE_BUCKET_OLD,
                                     entry.name)
                 except boto.exception.S3ResponseError, e:
                     self.stderr.write('ERROR {}'.format(e))
