504357393ad26a465952011a68b32c8e84fdc9f7
Kyle Conroy <kyle@kyleconroy.com>
2014-01-24 23:40:28 -0800

    Add command to backfill all games

--- Makefile
+++ Makefile
@@ -45,7 +45,10 @@ autoschema: venv
 
 flush: venv
 	. venv/bin/activate; python manage.py flush
-				
+
+makelove: venv
+	. venv/bin/activate; python manage.py makelove
+					
 deploy: test
 	git push origin master
 	git push heroku master
---
+++ games/management/commands/makelove.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+
+from games import models, bundle
+
+
+def package_love(stdout, game, release):
+    if release.get_asset('love') is not None:
+        stdout.write(u"SKIPPING {}".format(release))
+        return
+
+    upload = release.get_asset('uploaded')
+
+    if upload is None:
+        stdout.write(u"NO UPLOAD {}".format(release))
+        return
+
+    identity = bundle.detect_identity(upload.blob) or game.slug
+    config = bundle.game_config(game.uuid, identity, release.version)
+
+    prefix = "build/love8"
+
+    if release.love_version == "0.9.0":
+        prefix = "build/love9"
+
+    # Detect version, fail if not specified
+    love = bundle.inject_code(game, upload.blob, config)
+
+    slug = game.slug
+    name = game.name
+
+    # Create binaries
+    love_file = bundle.blobify(bundle.package_love, game, love, prefix,
+                               name, slug, release.version)
+
+    release.add_asset(love_file, tag='love')
+    stdout.write(u"FINISHED {}".format(release))
+
+
+class Command(BaseCommand):
+    help = 'Backfill LOVE files for all games'
+
+    def handle(self, *args, **options):
+
+        for game in models.Game.objects.all():
+            for release in game.release_set.all():
+                package_love(self.stdout, game, release)
